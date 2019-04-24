155e530ca07b22a35d3b27ac7aa6686d6f6499ff
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-04-24 21:55:14 +0200

    Added settingsfile sub and file writer sub

--- comparison.pl
+++ comparison.pl
@@ -4,8 +4,38 @@ use strict;
 use warnings;
 
 use Cwd;
+use Getopt::Long;
 
 my $dir = getcwd;
-
 my $javaArgs = " -cp $dir";
+
+#   params: engineCount | enginePath | scanParameter
+sub writeSettingsFile($$$)
+{
+    my ($engineCount, $filePath, $parameter) = $_;
+    my $settingsData = "";
+
+    for my $i (0 .. $engineCount) {
+        my $engineName = "[TestEngine$i]\n";
+        my $enginePath = "path=\"$filePath\"\n";
+        my $scanParameter = "scan_parameter=\"$parameter\"\n";
+        $settingsData += $engineName . $enginePath . $scanParameter;
+    }
+}
+
+#   params: filePath | data
+sub writeToFile($$)
+{
+    my ($filePath, $data) = $_;
+
+    printf "Started writing $filePath";
+
+    open(handle, '>', $filePath) or die $!;
+    print handle $data;
+    close(handle);
+
+    printf "Settings file written";
+}
+
+
 
