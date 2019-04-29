f56cc6c47ea74449566d8c76671efffcd273a5fb
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-04-29 14:41:08 +0200

    Fixed current issues with the script

--- comparison.pl
+++ comparison.pl
@@ -12,22 +12,38 @@ my $javaProjectName = undef;
 
 my $cPlusProjectName = undef;
 
+sub printUsage
+{
+    print "The parameters of the script are mandatory!\n";
+    print "\t-javaName=JavaProjectName\n";
+    print "\t-cPlusName=cPlusProjectName\n";
+
+    return;
+}
+
 GetOptions (
     "javaName=s"    => \$javaProjectName,
     "cPlusName=s"   => \$cPlusProjectName
 ) or die ("Error in command line arguments\n");
 
+END {
+    print "$javaProjectName\n$cPlusProjectName\n";
+}
+
+die(printUsage) unless ($javaProjectName && $cPlusProjectName);
+
 #   params: engineCount | enginePath | scanParameter
 sub writeSettingsFile($$$)
 {
-    my ($engineCount, $filePath, $parameter) = $_;
+    print "@_\n";
+    my ($engineCount, $filePath, $parameter) = @_;
     my $settingsData = "";
-
+    print "$engineCount $filePath $parameter\n";
     for my $i (0 .. $engineCount) {
         my $engineName = "[TestEngine$i]\n";
         my $enginePath = "path=\"$filePath\"\n";
         my $scanParameter = "scan_parameter=\"$parameter\"\n";
-        $settingsData += $engineName . $enginePath . $scanParameter;
+        $settingsData .= $engineName . $enginePath . $scanParameter;
     }
     writeToFile("$dir\\ $javaProjectName", $settingsData);
     writeToFile("$dir\\ $cPlusProjectName", $settingsData);
@@ -36,16 +52,16 @@ sub writeSettingsFile($$$)
 #   params: filePath | data
 sub writeToFile($$)
 {
-    my ($filePath, $data) = $_;
+    my ($filePath, $data) = @_;
 
-    printf "Started writing $filePath";
+    printf "Started writing $filePath\n";
 
-    open(HANDLE, '>', $filePath) or die $!;
-    print HANDLE $data;
-    close(HANDLE);
+    open(my $HANDLE, '>', $filePath) or die $!;
+    print $HANDLE $data;
+    close($HANDLE);
 
     printf "Settings file written";
 }
 
-writeSettingsFile(20, "C:\TC_ARTIFACTS\SpeedComparison\6\Testengine\TestEngines.exe", "-s");
+writeSettingsFile(20, "C:\\TC_ARTIFACTS\\SpeedComparison\\6\\TestEngine\\TestEngines.exe", "-s");
 
