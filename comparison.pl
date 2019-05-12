1718ba65e6f36a5c4fbd681b91910f92bef84240
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-05-12 20:20:05 +0200

    Settings writer mode

--- comparison.pl
+++ comparison.pl
@@ -5,38 +5,46 @@ use warnings;
 
 use Cwd;
 use Getopt::Long;
+use File::Path;
+use JSON::Parse;
 
 my $dir = getcwd;
 my $javaArgs = " -cp $dir";
 my $javaProjectName = undef;
 my $cPlusProjectName = undef;
 my $enginePathParameter = undef;
+my $engineCount = 8; #   Default is 8 because YOLO (Your only length's OKTA)
 
 sub printUsage
 {
     print "The parameters of the script are mandatory!\n";
     print "\t-javaName=JavaProjectName\n";
     print "\t-cPlusName=cPlusProjectName\n";
+    print "\t-engine=enginePathParameter\n";
+    print "\t-engineCount=engineCount\n";
 
     return;
 }
 
-GetOptions (
+GetOptions(
     "javaName=s"    => \$javaProjectName,
     "cPlusName=s"   => \$cPlusProjectName,
-    "engine=s"      => \$enginePathParameter
-) or die ("Error in command line arguments\n");
+    "engine=s"      => \$enginePathParameter,
+    "engineCount=i" => \$engineCount
+) or die("Error in command line arguments\n");
 
 END {
-    print "$javaProjectName\n$cPlusProjectName\n";
+    #   print "$javaProjectName $cPlusProjectName $enginePathParameter\n";
 }
+
+$SIG{'INT'} = sub {END};
 
 die(printUsage) unless ($javaProjectName && $cPlusProjectName && $enginePathParameter);
 
 #   params: engineCount | enginePath | scanParameter
-sub writeSettingsFile($$$)
+sub writeSettingsFile($$)
 {
-    my ($engineCount, $filePath, $parameter) = @_;
+    my ($filePath, $parameter) = @_;
     my $settingsData = "";
     for my $i (0 .. $engineCount) {
         my $engineName = "[TestEngine$i]\n";
@@ -44,11 +52,9 @@ sub writeSettingsFile($$$)
         my $scanParameter = "scan_parameter=\"$parameter\"\n";
         $settingsData .= $engineName . $enginePath . $scanParameter;
     }
-    mkdir("$dir\\"."$javaProjectName\\settings\\");
-    mkdir("$dir\\"."$cPlusProjectName\\settings\\");
 
-    writeToFile("$dir\\"."$javaProjectName\\settings\\settings.ini", $settingsData);
-    writeToFile("$dir\\"."$cPlusProjectName\\settings\\settings.ini", $settingsData);
+    writeToFile("$dir/$javaProjectName/com/company/settings/settings.ini", $settingsData);
+    writeToFile("$dir/$cPlusProjectName/settings/settings.ini", $settingsData);
 }
 
 #   params: filePath | data
@@ -65,5 +71,5 @@ sub writeToFile($$)
     printf "Settings file written";
 }
 
-writeSettingsFile(20, $enginePathParameter, "-s");
+writeSettingsFile($enginePathParameter, "-s");
 
